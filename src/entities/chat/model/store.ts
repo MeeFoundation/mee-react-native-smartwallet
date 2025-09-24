a851471504d9cc12f47262d79b5d33c1e4580d0e
Dmitry Elovsky <delovsky@swiftinvention.com>
2025-09-24 18:33:16 +0500

    refactor: streamline paginated state management across chat, group, and person entities

--- src/entities/chat/model/store.ts
+++ src/entities/chat/model/store.ts
@@ -1,21 +1,18 @@
-import { atom, type Getter, type SetStateAction, type Setter, type WritableAtom } from 'jotai'
+import { atom, type Getter, type Setter } from 'jotai'
 import { atomFamily } from 'jotai/utils'
 import { isEqual } from 'lodash-es'
 
-import { DEFAULT_PAGE_SIZE } from '@/shared/config'
-import { AppError, UnknownError } from '@/shared/errors'
-import { INITIAL_PAGINATED_STATE, type PaginatedListState, type PaginationAction } from '@/shared/lib/paginated-list'
+import {
+  INITIAL_PAGINATED_STATE,
+  makeGetManagePaginatedStateAtom,
+  type PaginatedListState,
+  type PaginatedStateAtom,
+} from '@/shared/lib/paginated-list'
 
 import { getGroupChatMessages } from '../api/get-group-chat-messages'
 import { mockMeUser } from '../api/mock/chat'
 import { sendGroupChatMessage } from '../api/send-group-chat-message'
-import type {
-  ChatMessage,
-  ChatUser,
-  GetChatMessagesFetchParams,
-  GetChatMessagesPaginatedListFetchParams,
-  GetChatMessagesPaginatedListResponse,
-} from './types'
+import type { ChatMessage, ChatUser, GetChatMessagesFetchParams } from './types'
 
 export const currentUserAtom = atom<ChatUser>(mockMeUser)
 
@@ -23,79 +20,13 @@ type PaginatedChatMessagesListState = PaginatedListState<ChatMessage>
 
 export const getPaginatedChatMessagesListStateAtom = atomFamily<
   GetChatMessagesFetchParams,
-  WritableAtom<PaginatedChatMessagesListState, [SetStateAction<PaginatedChatMessagesListState>], void>
->((_params: GetChatMessagesFetchParams) => atom(INITIAL_PAGINATED_STATE), isEqual)
+  PaginatedStateAtom<ChatMessage>
+>(() => atom(INITIAL_PAGINATED_STATE), isEqual)
 
-export const getManagePaginatedChatMessagesListAtom = atomFamily((params: GetChatMessagesFetchParams) => {
-  const chatMessagesDataAtom = getPaginatedChatMessagesListStateAtom(params)
-
-  return atom(null, async (get, set, action: PaginationAction) => {
-    const currentState = get(chatMessagesDataAtom)
-
-    const fetchParams: GetChatMessagesPaginatedListFetchParams = {
-      ...params,
-      limit: DEFAULT_PAGE_SIZE,
-      startIndex: 0,
-    }
-
-    if (action === 'loadMore') {
-      /**
-       * If there is no next index, there is no more data to load
-       */
-      if (!currentState.data?.nextIndex) return
-      fetchParams.startIndex = currentState.data.nextIndex
-    }
-
-    /**
-     * Refresh sets the start index to 0
-     */
-    if (action === 'refresh') fetchParams.startIndex = 0
-
-    /**
-     * Reset sets the start index to 0
-     */
-    if (action === 'reset') fetchParams.startIndex = 0
-
-    set(chatMessagesDataAtom, (prev) => ({
-      ...prev,
-      isFetching: true,
-      isFetchingNextPage: action === 'loadMore',
-      isRefreshing: action === 'refresh' || action === 'reset',
-    }))
-
-    try {
-      const result = await getGroupChatMessages(fetchParams)
-
-      set(chatMessagesDataAtom, (prev) => {
-        /**
-         * Only load more adds data to the list
-         */
-        const newData: GetChatMessagesPaginatedListResponse =
-          action === 'loadMore' && prev.data ? { ...result, items: [...prev.data.items, ...result.items] } : result
-
-        return {
-          ...prev,
-          data: newData,
-          error: null,
-          hasNextPage: result.nextIndex !== null,
-          isFetched: true,
-          isFetching: false,
-          isFetchingNextPage: false,
-          isRefreshing: false,
-        } satisfies PaginatedChatMessagesListState
-      })
-    } catch (error) {
-      set(chatMessagesDataAtom, (prev) => ({
-        ...prev,
-        error: error instanceof AppError ? error : new UnknownError(),
-        isFetched: true,
-        isFetching: false,
-        isFetchingNextPage: false,
-        isRefreshing: false,
-      }))
-    }
-  })
-}, isEqual)
+export const getManagePaginatedChatMessagesListAtom = makeGetManagePaginatedStateAtom(
+  getPaginatedChatMessagesListStateAtom,
+  getGroupChatMessages,
+)
 
 type ChatSendMessageAction = {
   type: 'send_message'
--- src/shared/lib/paginated-list.ts
+++ src/shared/lib/paginated-list.ts
@@ -1,9 +1,13 @@
-import { type Atom, useAtomValue, useSetAtom, type WritableAtom } from 'jotai'
-import { useEffect } from 'react'
+import { type Atom, atom, type Getter, type Setter, useAtomValue, useSetAtom, type WritableAtom } from 'jotai'
+import { atomFamily } from 'jotai/utils'
+import { isEqual } from 'lodash-es'
+import { type SetStateAction, useEffect } from 'react'
 
-import type { AppError } from '@/shared/errors'
+import { AppError, UnknownError } from '@/shared/errors'
 
-import type { PaginatedListResponse } from '../model/api'
+import { DEFAULT_PAGE_SIZE } from '../config'
+import type { PaginatedFetchParams, PaginatedListResponse } from '../model/api'
+import { assertUnreachable } from './assert-unreachable'
 
 export type PaginationAction = 'loadMore' | 'refresh' | 'reset'
 
@@ -49,3 +53,120 @@ export const usePaginatedState = <TItem, TFetchParams>(
 
   return [state, manageList] as const
 }
+
+export type PaginatedStateAtom<TItem> = WritableAtom<
+  PaginatedListState<TItem>,
+  [SetStateAction<PaginatedListState<TItem>>],
+  void
+>
+
+const handleLoadMore = async <TItem>(
+  get: Getter,
+  set: Setter,
+  paginatedStateAtom: PaginatedStateAtom<TItem>,
+  fetchResponse: (params: PaginatedFetchParams) => PaginatedListResponse<TItem> | Promise<PaginatedListResponse<TItem>>,
+) => {
+  const currentState = get(paginatedStateAtom)
+
+  /**
+   * If there is no next index, there is no more data to load
+   */
+  if (!currentState.data?.nextIndex) return
+
+  set(paginatedStateAtom, (prev) => ({
+    ...prev,
+    isFetching: true,
+    isFetchingNextPage: true,
+  }))
+
+  const reponse = await fetchResponse({
+    limit: DEFAULT_PAGE_SIZE,
+    startIndex: currentState.data.nextIndex,
+  })
+
+  set(paginatedStateAtom, (prev) => ({
+    ...prev,
+    data: { ...reponse, items: [...(prev.data?.items ?? []), ...reponse.items] },
+    error: null,
+    hasNextPage: reponse.nextIndex !== null,
+    isFetched: true,
+    isFetching: false,
+    isFetchingNextPage: false,
+    isRefreshing: false,
+  }))
+}
+
+const handleRefresh = async <TItem>(
+  _get: Getter,
+  set: Setter,
+  paginatedStateAtom: PaginatedStateAtom<TItem>,
+  fetchResponse: (params: PaginatedFetchParams) => PaginatedListResponse<TItem> | Promise<PaginatedListResponse<TItem>>,
+) => {
+  set(paginatedStateAtom, (prev) => ({
+    ...prev,
+    isFetching: true,
+    isRefreshing: true,
+  }))
+
+  const reponse = await fetchResponse({
+    limit: DEFAULT_PAGE_SIZE,
+    startIndex: 0,
+  })
+
+  set(paginatedStateAtom, (prev) => ({
+    ...prev,
+    data: reponse,
+    error: null,
+    hasNextPage: reponse.nextIndex !== null,
+    isFetched: true,
+    isFetching: false,
+    isFetchingNextPage: false,
+    isRefreshing: false,
+  }))
+}
+
+export const handlePaginatedStateAtomAction = async <TItem>(
+  get: Getter,
+  set: Setter,
+  action: PaginationAction,
+  paginatedStateAtom: PaginatedStateAtom<TItem>,
+  fetchResponse: (params: PaginatedFetchParams) => PaginatedListResponse<TItem> | Promise<PaginatedListResponse<TItem>>,
+) => {
+  try {
+    switch (action) {
+      case 'loadMore':
+        return await handleLoadMore(get, set, paginatedStateAtom, fetchResponse)
+      case 'reset':
+      case 'refresh':
+        return await handleRefresh(get, set, paginatedStateAtom, fetchResponse)
+      default:
+        assertUnreachable(action)
+        return
+    }
+  } catch (error) {
+    set(paginatedStateAtom, (prev) => ({
+      ...prev,
+      error: error instanceof AppError ? error : new UnknownError(),
+      isFetched: true,
+      isFetching: false,
+      isFetchingNextPage: false,
+      isRefreshing: false,
+    }))
+  }
+}
+
+export const makeGetManagePaginatedStateAtom = <TItem, TFetchParams>(
+  getAtom: (params: TFetchParams) => PaginatedStateAtom<TItem>,
+  fetchResponse: (
+    params: PaginatedFetchParams & TFetchParams,
+  ) => PaginatedListResponse<TItem> | Promise<PaginatedListResponse<TItem>>,
+) =>
+  atomFamily(
+    (params: TFetchParams) =>
+      atom(null, async (get, set, action: PaginationAction) =>
+        handlePaginatedStateAtomAction(get, set, action, getAtom(params), (paginatedParams) =>
+          fetchResponse({ ...params, ...paginatedParams }),
+        ),
+      ),
+    isEqual,
+  )
